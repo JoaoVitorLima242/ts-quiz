6f00dbbc7edf7babb6a885582f7f11c9dd79fced
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-08 18:27:37 -0300

    Question Type

---
+++ src/helpers/types/index.ts
@@ -0,0 +1,14 @@
+export enum Difficulty {
+    EASY = 'easy',
+    MEDIUM = "medium",
+    HARD = "hard"
+}
+
+export type Question = {
+    category: string;
+    correctAnswer: string;
+    difficulty: string;
+    incorrectAnswer: string[];
+    question: string;
+    type: string;
+}
