ffafde953cfb54e9c5d4fe8c7bcdcee63f86c7e1
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-08 18:13:36 -0300

    adjusting Question Card props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState }from "react";
 // Components
 import QuestionCard from "./components/QuestionCard/QuestionCard";
 
 const App = () => {
-  
+
+  const totalValue: number = 10;
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [gameOver, setGameOver] = useState(true);
+
   const  startTrivia = async () => {
 
   }
@@ -22,7 +30,14 @@ const App = () => {
       <button className="start" onClick={startTrivia}>Start</button>
       <p className="score">Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard/>
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={totalValue}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}></button>
     </div>
   );
